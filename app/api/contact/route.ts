e57459d12cc16ed27461b5206ba47263727f19e1
André Tutas <tutankhamal@outlook.com>
2025-08-13 04:24:10 -0300

    fix nodemailer

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest, NextResponse } from "next/server"
-import * as nodemailer from "nodemailer"
+const nodemailer = require("nodemailer")
 
 export async function POST(request: NextRequest) {
   try {
