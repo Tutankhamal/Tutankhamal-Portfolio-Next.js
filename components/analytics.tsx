aaf56f63b6d05fda702b6abb2e73cd7ef9aaa738
André Tutas <tutankhamal@outlook.com>
2025-08-25 14:21:57 -0300

    fix google analytics

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,32 +1,22 @@
 'use client'
 
 import Script from 'next/script'
-import { useGoogleAnalytics } from '@/hooks/useGoogleAnalytics'
-import { GA_TRACKING_ID } from '@/lib/gtag'
 
 export default function Analytics() {
-  useGoogleAnalytics()
-
   return (
     <>
       <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-12DDY9N6FS"
+        strategy="afterInteractive"
       />
-      <Script
-        id="gtag-init"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-12DDY9N6FS');
+        `}
+      </Script>
     </>
   )
 }
