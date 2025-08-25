dd84e80b82b0ad85e8a2deceb9079224522df8ba
André Tutas <tutankhamal@outlook.com>
2025-08-25 12:29:44 -0300

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,28 @@ export const metadata: Metadata = {
   title: 'Tutankhamal Portfolio',
   description: 'Created by Tutankhamal',
   generator: 'Tutankhamal Dev',
+  openGraph: {
+    title: 'Tutankhamal Portfolio',
+    description: 'Creating innovative digital experiences with cutting-edge technologies',
+    url: 'https://tutankhamal.vercel.app',
+    siteName: 'Tutankhamal Portfolio',
+    images: [
+      {
+        url: 'https://qyucubbl29wl9pia.public.blob.vercel-storage.com/og-image.webp',
+        width: 600,
+        height: 240,
+        alt: 'Tutankhamal Portfolio',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Tutankhamal Portfolio',
+    description: 'Creating innovative digital experiences with cutting-edge technologies',
+    images: ['https://qyucubbl29wl9pia.public.blob.vercel-storage.com/og-image.webp'],
+  },
 }
 
 export default function RootLayout({
